3992491229dbde79d092b8be897a54539718d807
vt71ru <69967509+vt71ru@users.noreply.github.com>
2022-06-19 22:02:22 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env  bash
 
 echo -e "Begin......"
+directory=$(dirname "$(readlink -f "$0")") #git repository
+
+echo "Directory:    "$directory
+
+dir_config="${directory}"/etc/anarchy.conf
+echo "Directory for config files:    "$dir_config
+
+dir_scripts="${directory}"/lib
+echo "Directory for script files:    "$dir_scripts
