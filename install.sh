33a5f07eb42d41d29bd8d43ec5737230bed98efa
vt71ru <69967509+vt71ru@users.noreply.github.com>
2022-06-19 22:04:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,3 +10,10 @@ echo "Directory for config files:    "$dir_config
 
 dir_scripts="${directory}"/lib
 echo "Directory for script files:    "$dir_scripts
+
+for script in "${dir_scripts}"/*.sh ; do
+        [[ -e "${script}" ]] || break
+        # shellcheck source=/usr/lib/anarchy/*.sh
+        echo $script
+        source "${script}"
+done
