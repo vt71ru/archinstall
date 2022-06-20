d9056567e6c8699cb4c0c83ff563834492ef7fa8
vt71ru <69967509+vt71ru@users.noreply.github.com>
2022-06-20 12:00:49 +0300

    Update language.sh

--- lib/language.sh
+++ lib/language.sh
@@ -24,13 +24,13 @@ language() {
         "Swedish" "Svenska" 3>&1 1>&2 2>&3)
 
     case "$ILANG" in
-        "English") export lang_file="${directory}"/lang/english.conf ;;
-        "Bulgarian") export lang_file="${directory}"/lang/bulgarian.conf lib=bg bro=bg ;;
-        "Dutch") export lang_file="${directory}"/lang/dutch.conf lib=nl bro=nl ;;
-        "French") export lang_file="${directory}"/lang/french.conf lib=fr bro=fr ;;
-        "German") export lang_file="${directory}"/lang/german.conf lib=de bro=de ;;
-        "Greek") export lang_file="${directory}"/lang/greek.conf lib=el bro=el ;;
-        "Hungarian") export lang_file="${directory}"/lang/hungarian.conf lib=hu bro=hu ;;
+        "English") export lang_file="${directory}"/lang/english.lng ;;
+        "Bulgarian") export lang_file="${directory}"/lang/bulgarian.lng lib=bg bro=bg ;;
+        "Dutch") export lang_file="${directory}"/lang/dutch.lng lib=nl bro=nl ;;
+        "French") export lang_file="${directory}"/lang/french.lng lib=fr bro=fr ;;
+        "German") export lang_file="${directory}"/lang/german.lng lib=de bro=de ;;
+        "Greek") export lang_file="${directory}"/lang/greek.lng lib=el bro=el ;;
+        "Hungarian") export lang_file="${directory}"/lang/hungarian.lng lib=hu bro=hu ;;
         "Indonesian") export lang_file="${anarchy_directory}"/lang/indonesia.conf lib=id bro=id ;;
         "Italian") export lang_file="${directory}"/lang/italian.conf lib=it bro=it ;;
         "Latvian") export lang_file="${directory}"/lang/latvian.conf lib=lv bro=lv ;;
@@ -38,10 +38,10 @@ language() {
         "Polish") export lang_file="${directory}"/lang/polish.conf lib=pl bro=pl ;;
         "Portuguese") export lang_file="${directory}"/lang/portuguese.conf lib=pt bro=pt-pt ;;
         "Portuguese-Brazilian") export lang_file="${directory}"/lang/portuguese-br.conf lib=pt-br bro=pt-br ;;
-        "Romanian") export lang_file="${directory}"/lang/romanian.conf lib=ro bro=ro ;;
+        "Romanian") export lang_file="${directory}"/lang/romanian.lng lib=ro bro=ro ;;
         "Russian") export lang_file="${directory}"/lang/russian.lng lib=ru bro=ru ;;
-        "Spanish") export lang_file="${directory}"/lang/spanish.conf lib=es bro=es-es ;;
-        "Swedish") export lang_file="${directory}"/lang/swedish.conf lib=sv bro=sv-se ;;
+        "Spanish") export lang_file="${directory}"/lang/spanish.lng lib=es bro=es-es ;;
+        "Swedish") export lang_file="${directory}"/lang/swedish.lng lib=sv bro=sv-se ;;
     esac
 
 }
