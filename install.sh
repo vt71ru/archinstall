8c178019de2fcb6dce59ea1a91c0682eafe9fd82
vt71ru <69967509+vt71ru@users.noreply.github.com>
2022-06-20 11:30:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,14 +6,17 @@ directory=$(dirname "$(readlink -f "$0")") #git repository
 echo "Directory:    "$directory
 
 dir_config="${directory}"/etc/anarchy.conf
-echo "Directory for config files:    "$dir_config
-
+#echo "Directory for config files:    "$dir_config
 dir_scripts="${directory}"/lib
-echo "Directory for script files:    "$dir_scripts
+#echo "Directory for script files:    "$dir_scripts
 
 for script in "${dir_scripts}"/*.sh ; do
         [[ -e "${script}" ]] || break
         source "${script}"
 done
 
-first
+source "${dir_config}"
+language
+    # shellcheck source=/usr/share/anarchy/lang
+    source "${lang_file}" # /lib/language.sh:43-60
+    export reload=true
