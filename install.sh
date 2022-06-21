304a0d45db9e07b68c27aae93ee511a73b014c12
vt71ru <69967509+vt71ru@users.noreply.github.com>
2022-06-21 23:45:34 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env  bash
 
+init() {
+Begin...........................
 directory=$(dirname "$(readlink -f "$0")") #git repository
 
 echo "Directory:    "$directory
 
 dir_config="${directory}"/etc/install.conf
-#echo "Directory for config files:    "$dir_config
 dir_scripts="${directory}"/lib
-#echo "Directory for script files:    "$dir_scripts
 
 for script in "${dir_scripts}"/*.sh ; do
         [[ -e "${script}" ]] || break
@@ -17,8 +17,13 @@ done
 source "${dir_config}"
 config
 language
-# shellcheck source=/usr/share/anarchy/lang
-source "${lang_file}" # /lib/language.sh:43-60
+source "${lang_file}" 
 export reload=true
+}
 
+main() {
 set_locale
+}
+
+init
+main
