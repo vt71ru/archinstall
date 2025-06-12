ad72e52d0ca090eae9bab9bad77b3005fd84d5b6
vt71ru <69967509+vt71ru@users.noreply.github.com>
2025-06-12 16:41:26 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,35 +1,21 @@
 #!/usr/bin/env  bash
 
+BOOT_MODE=""
+
 init() {
 echo -e "Begin..............................."
-directory=$(dirname "$(readlink -f "$0")") #git repository
 
-echo "Directory:    "$directory
+# if `sys/firware/efi/efivars` directory exist, the 
+# boot mode is UEFI. otherwise,the boot mode is BIOS
+if [ -d "/sys/firmware/efi/efivars" ]
+then
+    BOOT_MODE="UEFI"
+    echo "Boot mode is UEFI"
+else
+    BOOT_MODE="BIOS"
+    echo "Boot mode is BIOS"
+fi
 
-dir_config="${directory}"/etc/install.conf
-dir_scripts="${directory}"/lib
-
-for script in "${dir_scripts}"/*.sh ; do
-        [[ -e "${script}" ]] || break
-        source "${script}"
-done
-
-source "${dir_config}"
-config
-language
-source "${lang_file}" 
-export reload=true
-}
-
-main() {
-echo $lang_file
-
-set_locale
-check_connect
-clear
-echo $zonelist
-set_tz
 }
 
 init
-main
