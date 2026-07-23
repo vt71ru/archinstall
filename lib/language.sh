b2da1f7e706d76ff553271b0e656bcd86c79a407
vt71ru <69967509+vt71ru@users.noreply.github.com>
2026-07-23 19:48:36 +0300

    Update language.sh

--- lib/language.sh
+++ lib/language.sh
@@ -5,7 +5,7 @@
 #
 
 ########################################
-# Языки установщика
+# Languages
 ########################################
 
 readonly LANGUAGES=(
@@ -18,35 +18,53 @@ readonly LANGUAGE_FILES=(
     "en.sh"
 )
 
+LANGUAGE_FILE="${LANGUAGE_FILE:-}"
+
 ########################################
-# Выбор языка
+# Language selection
 ########################################
 
 select_language()
 {
+    local index
+
     section "Язык установщика"
 
     menu_select \
         "Выберите язык" \
         "${LANGUAGES[@]}"
 
-    LANGUAGE_FILE="${LANGUAGE_FILES[$((REPLY-1))]}"
+    index=$((REPLY - 1))
+
+    [[ -n "${LANGUAGE_FILES[$index]:-}" ]] \
+        || die "Некорректный выбор языка"
+
+    LANGUAGE_FILE="${LANGUAGE_FILES[$index]}"
 
     export LANGUAGE_FILE
+
+
+    msg_success "Выбран язык: ${LANGUAGES[$index]}"
 }
 
 ########################################
-# Загрузка языка
+# Language loading
 ########################################
 
 load_language()
 {
-    local file="${LANG_DIR}/${LANGUAGE_FILE}"
+    local file
+
+    [[ -n "${LANGUAGE_FILE:-}" ]] \
+        || die "Язык не выбран"
+
+    file="${LANG_DIR}/${LANGUAGE_FILE}"
 
     [[ -f "$file" ]] \
-        || return 1
+        || die "Файл языка отсутствует: $file"
 
+    # shellcheck disable=SC1090
     source "$file"
 
-    msg_success "Язык загружен"
+    msg_success "Язык загружен: $LANGUAGE_FILE"
 }
