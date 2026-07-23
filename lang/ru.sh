6352d8e8c4c19a9f811b2bd79566e3e245a3224e
vt71ru <69967509+vt71ru@users.noreply.github.com>
2026-07-23 20:34:40 +0300

    Update ru.sh

--- lang/ru.sh
+++ lang/ru.sh
@@ -1,34 +1,42 @@
-```bash
 #!/usr/bin/env bash
 #
-# Русская локализация ArchInstaller
+# ArchInstaller
+# Russian language
 #
 
-########################################
-# Название языка
-########################################
+[[ -n "${ARCHINSTALLER_RU_LOADED:-}" ]] && return
+readonly ARCHINSTALLER_RU_LOADED=1
 
-readonly LANG_NAME="Русский"
-readonly LANG_CODE="ru"
 
 ########################################
-# Цветной вывод сообщений
+# Language information
+########################################
+
+LANG_NAME="Русский"
+LANG_CODE="ru"
+
+
+########################################
+# Messages
 ########################################
 
 msg_info()
 {
-    printf "\e[34m[ИНФО]\e[0m %s\n" "$*"
+    printf "\e[36m[ИНФО]\e[0m %s\n" "$*"
 }
+
 
 msg_success()
 {
     printf "\e[32m[ OK ]\e[0m %s\n" "$*"
 }
 
+
 msg_warning()
 {
     printf "\e[33m[WARN]\e[0m %s\n" "$*"
 }
+
 
 msg_error()
 {
@@ -36,79 +44,49 @@ msg_error()
 }
 
 ########################################
-# Общие строки
+# Interface
 ########################################
 
-readonly TXT_YES="Да"
-readonly TXT_NO="Нет"
-readonly TXT_BACK="Назад"
-readonly TXT_EXIT="Выход"
-readonly TXT_CANCEL="Отмена"
-readonly TXT_CONTINUE="Продолжить"
+TXT_YES="Да"
+TXT_NO="Нет"
+
+TXT_BACK="Назад"
+TXT_EXIT="Выход"
+TXT_CANCEL="Отмена"
+TXT_CONTINUE="Продолжить"
 
 ########################################
-# Заголовки
+# Titles
 ########################################
 
-readonly TITLE_WELCOME="Добро пожаловать в ArchInstaller"
-readonly TITLE_LANGUAGE="Выбор языка"
-readonly TITLE_LOCALE="Настройка локали"
-readonly TITLE_NETWORK="Проверка сети"
-readonly TITLE_DISK="Выбор диска"
-readonly TITLE_PARTITION="Разметка диска"
-readonly TITLE_INSTALL="Установка системы"
-readonly TITLE_BOOTLOADER="Установка загрузчика"
-readonly TITLE_USERS="Создание пользователя"
-readonly TITLE_FINISH="Завершение установки"
+TITLE_LANGUAGE="Выбор языка"
+TITLE_NETWORK="Проверка сети"
+TITLE_LOCALE="Настройка локали"
+TITLE_DISK="Выбор диска"
+TITLE_PARTITION="Разметка диска"
+TITLE_INSTALL="Установка системы"
+TITLE_BOOTLOADER="Загрузчик"
+TITLE_USERS="Пользователи"
+TITLE_FINISH="Завершение"
 
 ########################################
-# Сообщения
+# Messages
 ########################################
 
-readonly MSG_LOADING_MODULES="Загрузка модулей..."
-readonly MSG_CHECKING_COMMANDS="Проверка необходимых команд..."
-readonly MSG_CHECKING_ENVIRONMENT="Проверка окружения..."
-readonly MSG_CHECKING_NETWORK="Проверка подключения к Интернету..."
-readonly MSG_SELECT_DISK="Выберите диск для установки."
-readonly MSG_SELECT_METHOD="Выберите способ разметки диска."
-readonly MSG_PREPARING_DISKS="Подготовка разделов..."
-readonly MSG_INSTALLING_SYSTEM="Установка базовой системы..."
-readonly MSG_INSTALLING_BOOTLOADER="Установка загрузчика..."
-readonly MSG_CREATING_USERS="Создание пользователей..."
-readonly MSG_FINISH="Установка успешно завершена."
-readonly MSG_REBOOT="Теперь можно перезагрузить компьютер."
+MSG_LOADING_MODULES="Загрузка модулей..."
+MSG_CHECKING_COMMANDS="Проверка команд..."
+MSG_CHECKING_ENVIRONMENT="Проверка окружения..."
+MSG_CHECKING_NETWORK="Проверка сети..."
+
+MSG_INSTALLING_SYSTEM="Установка системы..."
+MSG_FINISH="Установка завершена."
 
 ########################################
-# Подтверждения
+# Errors
 ########################################
 
-readonly ASK_CONTINUE="Продолжить?"
-readonly ASK_FORMAT="Все данные на выбранном диске будут уничтожены. Продолжить?"
-readonly ASK_REBOOT="Перезагрузить компьютер сейчас?"
-
-########################################
-# Ошибки
-########################################
-
-readonly ERR_ROOT="Установщик должен быть запущен от имени root."
-readonly ERR_NETWORK="Отсутствует подключение к Интернету."
-readonly ERR_DISK="Не удалось определить диск."
-readonly ERR_PARTITION="Ошибка разметки диска."
-readonly ERR_INSTALL="Ошибка установки системы."
-readonly ERR_BOOTLOADER="Ошибка установки загрузчика."
-readonly ERR_USER="Ошибка создания пользователя."
-readonly ERR_UNKNOWN="Неизвестная ошибка."
-
-########################################
-# Успешные действия
-########################################
-
-readonly OK_ENVIRONMENT="Окружение проверено."
-readonly OK_NETWORK="Сетевое подключение активно."
-readonly OK_PARTITION="Разделы успешно созданы."
-readonly OK_INSTALL="Базовая система установлена."
-readonly OK_BOOTLOADER="Загрузчик установлен."
-readonly OK_USER="Пользователь создан."
-readonly OK_FINISH="Установка завершена успешно."
-```
-
+ERR_ROOT="Требуются права root."
+ERR_NETWORK="Нет подключения к сети."
+ERR_DISK="Диск не найден."
+ERR_INSTALL="Ошибка установки."
+ERR_UNKNOWN="Неизвестная ошибка."
