97cdb6f7d0c8c03eef09c8a39b3562f3db627ece
vt71ru <69967509+vt71ru@users.noreply.github.com>
2026-07-23 13:13:52 +0300

    Update locale.sh

--- lib/locale.sh
+++ lib/locale.sh
@@ -1,1 +1,63 @@
+#!/usr/bin/env bash
+#
+# ArchInstaller
+# System locale module
+#
 
+########################################
+# Список локалей
+########################################
+
+LOCALES=()
+
+########################################
+# Загрузка локалей
+########################################
+
+load_locales()
+{
+    mapfile -t LOCALES < <(
+        sed -n \
+        's/^#\([A-Za-z_]*\.UTF-8\).*/\1/p' \
+        /etc/locale.gen
+    )
+}
+
+########################################
+# Выбор локали
+########################################
+
+select_locales()
+{
+    section "Системная локаль"
+
+    load_locales
+
+    menu_select \
+        "Выберите локаль" \
+        "${LOCALES[@]}"
+
+    SELECTED_LOCALE="${LOCALES[$((REPLY-1))]}"
+
+    export SELECTED_LOCALE
+}
+
+########################################
+# Настройка локали
+########################################
+
+configure_locale()
+{
+    local root="${1:-/mnt}"
+
+    sed -i \
+        "s/^#${SELECTED_LOCALE}/${SELECTED_LOCALE}/" \
+        "${root}/etc/locale.gen"
+
+    arch-chroot "$root" locale-gen
+
+    echo "LANG=${SELECTED_LOCALE}" \
+        > "${root}/etc/locale.conf"
+
+    msg_success "Локаль настроена"
+}
